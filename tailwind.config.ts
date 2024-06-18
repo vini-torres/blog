f06d800ec54a4d1edd325b66de0ac656a9577ee3
vini-torres <vinicius.carlosoficial@outlook.com>
2024-06-18 14:18:15 -0300

    "refactor: changing code configuration"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/**/*.{html,tsx,ts}'],
+  darkMode: 'class',
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      white: '#FFF',
+      black: '#000',
+      light: {
+        100: '#E5E7EB',
+        200: '#F2F2F2',
+        300: '#C8C8C8',
+        400: '#636363',
+        500: '#666666',
+        600: '#787878',
+        700: '#3D3D3D',
+        800: '#27272a',
+        900: '#18181b',
+      },
+      dark: {
+        100: '#E5E5E5',
+        200: '#898989',
+        300: '#202020',
+        400: '#222222',
+        500: '#262626',
+        600: '#171717',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config

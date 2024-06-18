4c6fe1de6025db9e059dc955696f4bb4389e2f75
vini-torres <vinicius.carlosoficial@outlook.com>
2024-06-18 17:04:44 -0300

    "feat: creating auth page"

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,3 +1,14 @@
+import SignIn from '@/components/SignIn'
+
 export default function Auth() {
-  return <h1 className="">Auth</h1>
+  return (
+    <section className="flex h-screen w-full flex-col items-center justify-center space-y-10">
+      <span>Logo</span>
+      <div className="flex flex-col items-center justify-center">
+        <h2 className="text-4xl font-semibold text-dark-500">Welcome back!</h2>
+        <p className="text-light-400">Sign in to your account</p>
+        <SignIn className="mt-6 w-72" />
+      </div>
+    </section>
+  )
 }
---
+++ src/components/SignIn/index.tsx
@@ -0,0 +1,20 @@
+import cx from 'classnames'
+import Image from 'next/image'
+
+interface SignInProps {
+  className?: string
+}
+
+export default function SignIn({ className }: SignInProps) {
+  return (
+    <button
+      className={cx(
+        'flex h-10 min-w-52 items-center justify-center gap-[0.625rem] rounded border border-dark-100 px-3 text-sm text-light-400 transition-colors duration-200 hover:bg-light-100',
+        className,
+      )}
+    >
+      <Image src="../google.svg" width={20} height={20} alt="Icon Google" />
+      Sign in with Google
+    </button>
+  )
+}

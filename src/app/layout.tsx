631d3b74d371d807285e7aedd86a00f24ac711f2
vini-torres <vinicius.carlosoficial@outlook.com>
2024-06-18 14:18:55 -0300

    "feat: creating project routes"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import "./globals.css";
+import './globals.css'
 
-import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
 
-const font = Nunito({ subsets: ["latin"] });
+const font = Nunito({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Blog",
-};
+  title: 'Blog',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="pt-br">
       <body className={font.className}>{children}</body>
     </html>
-  );
+  )
 }
